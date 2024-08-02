a02e227758a532fe170fc4a80216f0047b136bb4
Jiwoong <dnd0311@naver.com>
2024-08-02 17:46:08 +0900

    stage clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [ProductModule, OrderModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    ProductModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,23 @@
-export class Product {}
+import { Column, Entity } from 'typeorm';
+import BaseEntity from '../../common/base.entity';
+
+@Entity()
+export class Product extends BaseEntity {
+  @Column()
+  public name: string;
+
+  @Column()
+  public description: string;
+
+  @Column()
+  public productImg: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public stock: number;
+
+  @Column({ default: true })
+  public isSale: boolean;
+}
